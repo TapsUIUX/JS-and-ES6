0134429fcc3095e292a5792a1be1f09c18e7b7c3
TapsUIUX <25495356+TapsUIUX@users.noreply.github.com>
2017-10-31 16:37:24 -0400

    Update ArrayCheck.js

--- Array-Check-One-In-Other/ArrayCheck.js
+++ Array-Check-One-In-Other/ArrayCheck.js
@@ -23,3 +23,18 @@ var check = function(Aarray,Barray){
  
 
 console.log(check([5,8,1,8 ],[1,2,5,8,30]));
+
+
+
+
+ arr1 = [1,7,3];
+  arr2 = [1,2,3,4];
+function check(arr1,arr2){
+   var stat = 1;
+   arr1.forEach( (el,id,arr)=>{     
+   if (  arr2.some((el2,id2,ar2)=>(el2==el)) == false )
+   { stat = 0 ;}  }) //forEach-ends ;
+  if (stat==0){return false;}else {return true}
+  }//check-ends
+
+ console.log(check(arr1,arr2))
