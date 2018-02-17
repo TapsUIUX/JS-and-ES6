05ac0980b6c5c9b21c09b2cd47b7a0f8da7e0ce5
TapsUIUX <25495356+TapsUIUX@users.noreply.github.com>
2018-02-17 08:40:20 -0500

    Update HTMLfromJS.js
    Experimentals

--- ES6creatHtmls/HTMLfromJS.js
+++ ES6creatHtmls/HTMLfromJS.js
@@ -1,1 +1,23 @@
 
+  //experiments stuffs
+ (function(){
+     "use strict";
+     var section = document.querySelector(section);
+     
+     document.body.onload = addElement;
+
+     function addElement () { 
+  // create a new div element 
+  var newDiv = document.createElement("div"); 
+  // and give it some content 
+  var  Content = `<div style="color:red">hello</div>`
+  //document.createTextNode("Hi there and greetings!"); 
+  // add the text node to the newly created div
+  newDiv.innerHTML=(content);  
+
+  // add the newly created element and its content into the DOM 
+  var currentDiv = document.getElementById("div1"); 
+  document.body.insertBefore(newDiv, currentDiv); 
+} 
+      
+ })()
