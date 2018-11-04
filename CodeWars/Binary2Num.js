bd0609998fe2930f810b155b3d85b42baab40069
TapsUIUX <25495356+TapsUIUX@users.noreply.github.com>
2018-11-04 03:32:59 -0500

    Update Binary2Num.js

--- CodeWars/Binary2Num.js
+++ CodeWars/Binary2Num.js
@@ -4,3 +4,9 @@ const binaryArrayToNumber = arr => {
     return acc = acc + el* Math.pow(2,id)
   },0)
 };
+
+const binaryArrayToNumber = arr => parseInt(arr.join(''), 2);
+
+function binaryArrayToNumber(arr) {
+  return arr.reduce( (a, b) => a << 1 | b );
+}
